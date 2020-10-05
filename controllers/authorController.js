20105a09ef99e708da9818b6df7ec6ae849185d8
RitikPPatel05 <ritikpatel05121999@gmail.com>
2020-10-05 20:15:39 +0530

    Added author form post controllers and template of it.

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,5 +1,7 @@
 let Author = require('../models/author');
 let Book = require('../models/book');
+let validator = require('express-validator');
+const { check, body } = require('express-validator');
 
 // Display list of all Authors.
 exports.author_list = async (req, res, next) => {
@@ -30,13 +32,72 @@ exports.author_detail = async (req, res) => {
 
 // Display Author create form on GET.
 exports.author_create_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author create GET');
+    res.render('author_form', { title: 'Create Author '} );
 };
 
 // Handle Author create on POST.
-exports.author_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author create POST');
-};
+exports.author_create_post = [
+
+    // Validate fields.
+    check('first_name').
+        isLength({ min: 1} ).trim().withMessage('First Name must be specified.')
+        .isAlphanumeric().withMessage('First Name has non-alphanumeric characters.') ,
+    check('family_name')
+        .isLength({ min: 1} ).trim().withMessage('Family Name must be specified.')
+        .isAlphanumeric().withMessage('Family Name has non-alphanumeric characters.') ,
+    // We can use the optional() function to run a subsequent validation only if a field has been entered (this allows us to validate optional fields).
+    // Adds a validator to check for the existence of the current fields in the request. This means the value of the fields may not be undefined; all other values are acceptable.
+
+    // You can customize this behavior by passing an object with the following options:
+
+    // checkNull: if true, fields with null values will not exist
+    // checkFalsy: if true, fields with falsy values (eg "", 0, false, null) will also not exist
+    check('date_of_birth', 'Invalid date of birth' )
+        .optional({ checkFalsy: true }).isISO8601() ,
+    check('date_of_death', 'Invalid date of death')
+        .optional({ checkFalsy: true }).isISO8601(),
+
+
+    // Sanitize fields.
+    body('first_name').escape(),
+    body('family_name').escape(),
+    body('date_of_birth').toDate(),
+    body('date_of_death').toDate(),
+
+    // Process request after validation and sanitization.
+    (req, res, next) => {
+
+        // Extract the validation errors from a request.
+        const errors = validationResult(req);
+
+        if(!errors.isEmpty()) {
+            // There are errors. Render form again wwith sanitized values/errors messages.
+            res.render('author_form', { title: 'Create Author', author: req.body, errors: errors.array() });
+            return;
+        } else {
+            // Data from form is valid.
+
+            // Unlike with the Genre post handler, we don't check whether the Author object already exists before saving it. Arguably we should, though as it is now we can have multiple authors with the same name.
+
+            // Create an Author object with escpaed and trimmed data.
+            let author = new Author(
+                {
+                    first_name: req.body.first_name,
+                    family_name: req.body.family_name,
+                    date_of_birth: req.body.date_of_birth,
+                    date_of_death: req.body.date_of_death
+                });
+            
+            author.save()
+                .then( () => 
+                    // Sccessful - redirect to new author record.
+                    res.redirect(author.url)
+                )
+                .catch( err => next(err) )
+        }
+    }
+
+]
 
 // Display Author delete form on GET.
 exports.author_delete_get = function(req, res) {
